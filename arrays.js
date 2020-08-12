051d91821e327bc4df27186d84a16de1ff5b9d99
AnabelSalomone <anabel.salomone@gmail.com>
2020-08-12 19:58:57 +0200

    add methods

--- arrays.js
+++ arrays.js
@@ -73,3 +73,46 @@ const count = data.reduce((obj, transport) => {
     obj[transport] += 1
     return obj
 }, {});
+
+
+//*******************************************************************************************
+
+// ## Array Cardio Day 2
+
+const people2 = [
+  { name: 'Wes', year: 1988 },
+  { name: 'Kait', year: 1986 },
+  { name: 'Irv', year: 1970 },
+  { name: 'Lux', year: 2015 }
+];
+
+const comments = [
+  { text: 'Love this!', id: 523423 },
+  { text: 'Super good', id: 823423 },
+  { text: 'You are the best', id: 2039842 },
+  { text: 'Ramen is my fav food ever', id: 123523 },
+  { text: 'Nice Nice Nice!', id: 542328 }
+];
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+// Array.prototype.every() // is everyone 19 or older?
+var today = new Date();
+var year = today.getFullYear(); 
+
+const isEverybody19 = people2.every((element) => year - element.year >= 19)
+const isAnyone19 = people2.some((element) => year - element.year >= 19)
+
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+
+const findId = comments.find(comment => comment.id === 823423);
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
+
+const findIdIndex = comments.findIndex(element => element.id === 823423)
+comments.splice(findIdIndex, 1)
